4900d339a0c1965cd525e5650eda1e04061c95bf
taras-chernovol <chernovol.taras@gmail.com>
2016-09-27 14:51:07 +0300

    yourdoctor

--- site/application/view/menu.php
+++ site/application/view/menu.php
@@ -3,19 +3,19 @@
 		<a class="item active" href="/">Главная</a>
 		<a class="item" href="/services">Услуги</a>
 		<a class="item" href="/contacts">Контакты</a>
-		<a id="facebook" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
+		<a id="facebook" target="_blank" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
 	<?php endif ?>
 	<?php if (Application::$route == '/services'): ?>
 		<a class="item" href="/">Главная</a>
 		<a class="item active" href="/services">Услуги</a>
 		<a class="item" href="/contacts">Контакты</a>
-		<a id="facebook" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
+		<a id="facebook" target="_blank" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
 	<?php endif ?>
 	<?php if (Application::$route == '/contacts'): ?>
 		<a class="item" href="/">Главная</a>
 		<a class="item" href="/services">Услуги</a>
 		<a class="item active" href="/contacts">Контакты</a>
-		<a id="facebook" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
+		<a id="facebook" target="_blank" href="https://www.facebook.com/fz.novik?fref=ts" class="fa fa-facebook-square"></a>
 	<?php endif ?>
 </div>
 <div class="clear"></div>
