af95d6f026c7a31100470672a37125d4989c65a5
taras-chernovol <chernovol.taras@gmail.com>
2016-09-28 19:27:54 +0300

    yourdoctor

--- site/application/controller/contacts.php
+++ site/application/controller/contacts.php
@@ -19,5 +19,5 @@
 </div>
 <div class="clear"></div>
 <div id="googleMap">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d659.0476899999712!2d35.26092992926738!3d48.644465798704225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d945029e69d2f1%3A0xde8c0e9d17b6e592!2z0LLRg9C70LjRhtGPINCT0LXRgtGM0LzQsNC90YHRjNC60LAsIDEwMCwg0J3QvtCy0L7QvNC-0YHQutC-0LLRgdGM0LosINCU0L3RltC_0YDQvtC_0LXRgtGA0L7QstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1MTIwMA!5e0!3m2!1sru!2sua!4v1474982571507" width="800" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d659.0476899999712!2d35.26092992926738!3d48.644465798704225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d945029e69d2f1%3A0xde8c0e9d17b6e592!2z0LLRg9C70LjRhtGPINCT0LXRgtGM0LzQsNC90YHRjNC60LAsIDEwMCwg0J3QvtCy0L7QvNC-0YHQutC-0LLRgdGM0LosINCU0L3RltC_0YDQvtC_0LXRgtGA0L7QstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1MTIwMA!5e0!3m2!1sru!2sua!4v1474982571507" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
