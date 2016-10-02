b75be1b10e00cd8e6652b6d18c761bed2fe941dc
taras-chernovol <chernovol.taras@gmail.com>
2016-10-02 04:41:37 +0300

    YourDoctor

--- site/application/controller/contacts.php
+++ site/application/controller/contacts.php
@@ -1,25 +1,25 @@
 <div id="contactsWrapper">
 	<div class="contacts">
-		<i class="fa fa-phone"></i><span class="title">Контакты</span>
+		<i class="fa fa-phone"></i><span class="paddingRight10px"></span><span class="title">Контакты</span>
 		<span class="line">(067) 454-21-99</span>
 		<span class="line">(050) 451-53-08 </span>
 		<span class="e-mail">fz_novik@mail.ru</span>
 	</div>
 	<div class="contacts">
-		<i class="fa fa-clock-o"></i><span class="title">График работы</span>
+		<i class="fa fa-clock-o"></i><span class="paddingRight10px"></span><span class="title">График работы</span>
 		<span class="line">Пн-Пт: с 16.00 до 18.00</span>
 		<span class="line">Сб: с 10.00 до 12.00</span>
 		<span class="line">Вс: выходной </span>
 		<span class="line">Предварительная запись обязательна</span>
 	</div>
 	<div class="contacts">
-		<i class="fa fa-map-marker"></i><span class="title">Адрес</span>
+		<i class="fa fa-map-marker"></i><span class="paddingRight10px"></span><span class="title">Адрес</span>
 		<span class="line">г. Новомосковск,</span>
 		<span class="line">ул. Гетьманская,</span>
 		<span class="line">дом 100</span>
 	</div>
-</div>
-<div class="clear"></div>
-<div id="googleMap">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d659.0476899999712!2d35.26092992926738!3d48.644465798704225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d945029e69d2f1%3A0xde8c0e9d17b6e592!2z0LLRg9C70LjRhtGPINCT0LXRgtGM0LzQsNC90YHRjNC60LAsIDEwMCwg0J3QvtCy0L7QvNC-0YHQutC-0LLRgdGM0LosINCU0L3RltC_0YDQvtC_0LXRgtGA0L7QstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1MTIwMA!5e0!3m2!1sru!2sua!4v1474982571507" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<div class="clear"></div>
+	<div id="googleMap">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d659.0476899999712!2d35.26092992926738!3d48.644465798704225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d945029e69d2f1%3A0xde8c0e9d17b6e592!2z0LLRg9C70LjRhtGPINCT0LXRgtGM0LzQsNC90YHRjNC60LAsIDEwMCwg0J3QvtCy0L7QvNC-0YHQutC-0LLRgdGM0LosINCU0L3RltC_0YDQvtC_0LXRgtGA0L7QstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1MTIwMA!5e0!3m2!1sru!2sua!4v1474982571507" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
 </div>
--- site/application/controller/services.php
+++ site/application/controller/services.php
@@ -1,6 +1,6 @@
 <ul>
-	<li><span class="fa fa-circle"></span><span class="paddingRight5px"></span>Диагностика и лечение кожных заболеваний</li>
-	<li><span class="fa fa-circle"></span><span class="paddingRight5px"></span>Диагностика и лечение инфекций передаваемых половым путем, в том числе – экспресс-диагностика </li>
-	<li><span class="fa fa-circle"></span><span class="paddingRight5px"></span>Дерматоскопия и удаление доброкачественных образований кожи (мозолей, папилом, бородавок, невусов и т.п.)</li>
-	<li><span class="fa fa-circle"></span><span class="paddingRight5px"></span>Вызов врача на дом</li>
+	<li><i class="fa fa-circle"></i><span class="paddingRight10px"></span>Диагностика и лечение кожных заболеваний</li>
+	<li><i class="fa fa-circle"></i><span class="paddingRight10px"></span>Диагностика и лечение инфекций передаваемых половым путем, в том числе – экспресс-диагностика </li>
+	<li><i class="fa fa-circle"></i><span class="paddingRight10px"></span>Дерматоскопия и удаление доброкачественных образований кожи (мозолей, папилом, бородавок, невусов и т.п.)</li>
+	<li><i class="fa fa-circle"></i><span class="paddingRight10px"></span>Вызов врача на дом</li>
 </ul>
